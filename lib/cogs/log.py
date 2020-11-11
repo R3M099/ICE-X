f6af2b6e0bc9c600206d20c85a4e6b2220a4ed16
R3M099 <soumyadeepkanjilal1234@gmail.com>
2020-11-11 11:53:04 +0530

    update log.py

--- lib/cogs/log.py
+++ lib/cogs/log.py
@@ -15,19 +15,14 @@ class Log(Cog):
 	@Cog.listener()
 	async def on_user_update(self, before, after):
 		t = datetime.datetime.utcnow()
-		channel = get(after.guild.channels, name = "moderation-logs")
-		if channel is not None:
-			if before.avatar_url != after.avatar_url:
-				e = Embed(colour = 0xc720e5)
-				e.set_author(name = f"{after.display_name}#{after.discriminator}", icon_url = before.avatar_url)
-				e.set_thumbnail(url = after.avatar_url)
-				e.add_field(name = "Avatar Update", value = after.mention, inline = False)
-				e.set_footer(text = t.strftime('%d/%b/%Y | %I:%M %p UTC'))
-		
-				await channel.send(embed = e)
-
-		else:
-			pass
+		if before.avatar_url != after.avatar_url:
+			e = Embed(colour = 0xc720e5)
+			e.set_author(name = f"{after.display_name}#{after.discriminator}", icon_url = before.avatar_url)
+			e.set_thumbnail(url = after.avatar_url)
+			e.add_field(name = "Avatar Update", value = after.mention, inline = False)
+			e.set_footer(text = t.strftime('%b %d, %Y | %I:%M %p UTC'))
+	
+			await self.bot.get_channel(736790701794132029).send(embed = e)
 
 	@Cog.listener()
 	async def on_member_update(self, before, after):
@@ -44,7 +39,7 @@ class Log(Cog):
 				for name, value, inline in fields:
 					e.add_field(name = name, value = value, inline = inline)
 				e.set_thumbnail(url = after.avatar_url)
-				e.set_footer(text = t.strftime('%d/%b/%Y | %I:%M %p UTC'))
+				e.set_footer(text = t.strftime('%b %d, %Y | %I:%M %p UTC'))
 				
 				await channel.send(embed = e)
 
@@ -58,7 +53,7 @@ class Log(Cog):
 		if channel is not None:
 			if not after.author.bot:
 				if before.content != after.content:
-					e = Embed(description = f"Message Edited in `{after.channel}`", 
+					e = Embed(description = f"Message Edited in {after.channel.mention}", 
 							  colour = 0xdef00e)
 					fields = [("Before:", before.content, False),
 						  	  ("After:", after.content, False)]
@@ -66,7 +61,7 @@ class Log(Cog):
 					for name, value, inline in fields:
 						e.add_field(name = name, value = value, inline = inline)
 					e.set_author(name = f"{after.author.display_name}#{after.author.discriminator}", icon_url = after.author.avatar_url)
-					e.set_footer(text = t.strftime('%d/%b/%Y | %I:%M %p UTC'))
+					e.set_footer(text = t.strftime('%b %d, %Y | %I:%M %p UTC'))
 
 					
 					await channel.send(embed = e)
@@ -82,14 +77,14 @@ class Log(Cog):
 		channel = get(message.guild.channels, name = "moderation-logs")
 		if channel is not None:
 			if not message.author.bot:
-				e = Embed(description = f"Message sent by `{message.author.display_name}` Deleted in `{message.channel}`",
+				e = Embed(description = f"Message sent by {message.author.mention} Deleted in {message.channel.mention}",
 					  	  colour = 0xf00e11)
 				fields = [("Message content", message.content, False)]
 
 				for name, value, inline in fields:
 					e.add_field(name = name, value = value, inline = inline)
 				e.set_author(name = f"{message.author.display_name}#{message.author.discriminator}", icon_url = message.author.avatar_url)	
-				e.set_footer(text = t.strftime('%d/%b/%Y | %I:%M %p UTC'))
+				e.set_footer(text = t.strftime('%b %d, %Y | %I:%M %p UTC'))
 				
 				await channel.send(embed = e)
 
