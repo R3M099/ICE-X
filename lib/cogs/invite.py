c85eaa9521d329933dd2c13d90a3affab70b8645
R3M099 <soumyadeepkanjilal1234@gmail.com>
2020-11-17 14:51:56 +0530

    added source code command

--- lib/cogs/invite.py
+++ lib/cogs/invite.py
@@ -18,6 +18,18 @@ class Invite(Cog):
 
 		await ctx.send(embed = e)
 
+	@command(name = "source")
+	async def source_code(self, ctx):
+
+		t = datetime.datetime.utcnow()
+		e = Embed(description = f"{ctx.author.mention}, here's the source code for the bot :slight_smile:",
+				  colour = 0x0cb44e)
+		e.set_author(name = f"{ctx.author.display_name}#{ctx.author.discriminator}", icon_url = ctx.author.avatar_url)
+		e.add_field(name = "Source Code", value = '**[Source](https://github.com/R3M099/ICE-X "Github repository of the bot")**')
+		e.set_footer(text = f"Requested by {ctx.author.display_name} | {t.strftime('%b %d, %Y | %I:%M %p UTC')}")
+
+		await ctx.send(embed = e)
+
 	@Cog.listener()
 	async def on_ready(self):
 		if not self.bot.ready:
