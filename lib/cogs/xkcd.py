7d509fb29c4675460e99cef88342cf7f49a4e190
R3M099 <soumyadeepkanjilal1234@gmail.com>
2020-11-17 14:51:22 +0530

    updated xkcd.py

--- lib/cogs/xkcd.py
+++ lib/cogs/xkcd.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import Cog, command, cooldown, BucketType
+from discord.ext.commands import MissingRequiredArgument, BadArgument
 from aiohttp import request
 from discord import Embed
 import random
@@ -10,15 +11,11 @@ class Xkcd(Cog):
 
 	@command(name = "xkcd")
 	@cooldown(1, 5, BucketType.user)
-	async def xkcd(self, ctx):
-		'''Gives the current xkcd comic'''
+	async def xkcd(self, ctx, number: int):
 		t = datetime.datetime.utcnow()
 
-		random_number = random.randint(1, 2386)
-		#await ctx.send(random_number)
-
-		xkcd_url = f"https://xkcd.com/{random_number}/info.0.json"
-
+		xkcd_url = f"https://xkcd.com/{number}/info.0.json"
+		
 		async with request("GET", xkcd_url, headers = {}) as response:
 			if response.status == 200:
 				data = await response.json()
@@ -30,7 +27,7 @@ class Xkcd(Cog):
 			if response.status == 200:
 				data = await response.json()
 				e = Embed(title = data["title"],
-						  description = data["transcript"],
+						  description = data["alt"],
 						  colour = ctx.author.colour)
 				e.set_author(name = f"{ctx.author.display_name}#{ctx.author.discriminator}", icon_url = ctx.author.avatar_url)
 				if image_link is not None:
@@ -40,6 +37,13 @@ class Xkcd(Cog):
 
 			else:
 				await ctx.send(f'API returned a {response.status} status')
+
+	@xkcd.error
+	async def xkcd_error(self, ctx, exc):
+		if isinstance(exc, MissingRequiredArgument):
+			await ctx.send("One or more required arguments are missing")
+		if isinstance(exc, BadArgument):
+			await ctx.send("number must be an integer")
 	
 	@Cog.listener()
 	async def on_ready(self):
